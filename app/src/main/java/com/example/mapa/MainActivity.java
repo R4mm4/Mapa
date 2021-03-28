f28dcb8bab9da4eb992e1e35abd9e742f243509e
R4mm4 <65689530+R4mm4@users.noreply.github.com>
2021-03-28 00:52:03 -0600

    Correcciones

--- app/src/main/java/com/example/mapa/MainActivity.java
+++ app/src/main/java/com/example/mapa/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -21,21 +22,30 @@ import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.maps.android.PolyUtil;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.mapa.Utils.GPS_controler;
 import com.example.mapa.Utils.Utils;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -53,23 +63,67 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     RequestQueue request;
     Location location;
     GPS_controler gpsTracker;
+    private static final int PERMISSIONS_REQUEST_CODE = 1001;
+    private static final int MY_PERMISSION_REQUEST_API = 1002;
+    String[] appPermissions = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gpsTracker = new GPS_controler(getApplicationContext());
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        request = Volley.newRequestQueue(getApplicationContext());
+
+        if (checkAndRequestPermissions()) {
+            // El permiso ya ha sido concedido.
+
+            // Obtain the SupportMapFragment and get notified when the
+            // map is ready to be used.
+            gpsTracker = new GPS_controler(getApplicationContext());
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+            request = Volley.newRequestQueue(getApplicationContext());
+        }
 
     }
+
+    public boolean checkAndRequestPermissions() {
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String perm : appPermissions) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    perm
+            ) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(perm);
+            }
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    listPermissionsNeeded.toArray(
+                            new String[listPermissionsNeeded.size()]
+                    ), PERMISSIONS_REQUEST_CODE
+            );
+            return false;
+        }
+        return true;
+    }
+
 
     @Override
     public void onMapClick(LatLng latLng) {
 
     }
 
+    Boolean actualPosition = true; // Indica si es la posición actual del usuario.
+    JSONObject jsonObject; // JSONObject que tiene la información para realizar el trazado de ruta.
+    Double longitudOrigen, latitudOrigen; // Contienen la información de Longitud/Latitud de origen.
+
+    LatLng origen, destino; // Contienen la Longitud/Latitud de los marcadores Origen/Destino.
+    ArrayList markerPoints = new ArrayList(); // ArrayList que contiene los datos de los marcadores.
+    private FusedLocationProviderClient fusedLocationProviderClient;
     @Override
     public void onMapReady(GoogleMap googleMap) {
         nMap = googleMap;
@@ -83,19 +137,117 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        nMap.setMyLocationEnabled(true);
-        LatLng we= new LatLng(-12.198191,-77.013433);
-        nMap.animateCamera(CameraUpdateFactory.newLatLngZoom(we,10));
-        Utils.markersDefault(nMap,getApplicationContext());
-        nMap.setOnMapClickListener(this);
-        nMap.setOnMarkerClickListener(this);
+        nMap.setMyLocationEnabled(true); // Permite la localización del usuario.
 
+        /* Habilita la localización de servicios mediante el proveedor de cliente actual. */
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        /* Obtiene nuestra última localización. */
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(
+                this,
+                new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) { // Localización correcta.
+                        if (location != null) {
+                            latitudOrigen = location.getLatitude(); // Altitud localización act.
+                            longitudOrigen = location.getLongitude(); // Longitud loc. actual.
+                            actualPosition = false;
+
+                            /* Posición actual del usuario, añade los datos de Latitud/Longitud. */
+                            LatLng miPosicion = new LatLng(latitudOrigen, longitudOrigen);
+
+                            /* Cambia la posición geográfica de la camara a la del usuario. */
+                            CameraPosition cameraPosition = new CameraPosition.Builder()
+                                    .target(new LatLng(latitudOrigen, longitudOrigen))
+                                    .zoom(14)
+                                    .build();
+                            nMap.animateCamera(
+                                    CameraUpdateFactory.newCameraPosition(cameraPosition)
+                            );
+                        } else {
+                            /* Falló en obtener la ubicación del usuario. */
+                            Toast.makeText(
+                                    MainActivity.this,
+                                    "No se pudo obtener su ubicación...",
+                                    Toast.LENGTH_LONG
+                            ).show();
+                        }
+                    }
+                }
+        );
+
+        /* OnLongClickListener se encarga de agregar los marcadores en el sitio donde el
+         * usuario quiere obtener la ruta entre dos puntos. */
+        nMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            /**
+             * Obtiene la Longitud/Latitud del punto donde esta agregando el marcador.
+             * @param latLng Contiene los datos de Longitud/Latitud.
+             */
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                if (markerPoints.size() > 1) {
+                    markerPoints.clear();
+                    nMap.clear();
+                }
+
+                markerPoints.add(latLng); // Añade el marcador al Array de Points.
+
+                MarkerOptions options = new MarkerOptions();
+                options.position(latLng);
+
+                if (markerPoints.size() == 1) { // Marcador del punto de origen.
+                    nMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title("Marcador Origen")
+                            .snippet("Origen"));
+                } else if (markerPoints.size() == 2) { // Marcador del punto de destino.
+                    nMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title("Marcador Destino")
+                            .snippet("Destino"));
+                }
+
+                if (markerPoints.size() >= 2) { // Si ya existen dos puntos, traza la ruta.
+                    origen = (LatLng) markerPoints.get(0); // Latitud/Longitud marcador origen.
+                    destino = (LatLng) markerPoints.get(1); // Latitud/Longitud marcador destino.
+
+                    /* URL que utiliza la Longitud/Latitud de los puntos de origen y destino
+                     * y utiliza la API Key. */
+                    String url =
+                            "https://maps.googleapis.com/maps/api/directions/json?origin=" +
+                                    origen.latitude + "," + origen.longitude +
+                                    "&destination=" + destino.latitude + "," + destino.longitude +
+                                    "&key=AIzaSyC2-KpjjCwUXSpCLWh4mt4KRKFEPtGz4Rs";
+
+                    /* Se realiza un Request con Volley para interpretar los datos JSON del URL. */
+                    RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) { // Respuesta para trazar la ruta.
+                            try {
+                                jsonObject = new JSONObject(response);
+                                TrazarRuta(jsonObject);
+
+                                Log.i("JSONruta: ", response);
+                            } catch (JSONException e) { // No hubo respuesta, se produjo un error.
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() { // Error en el Volley.
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+
+                    queue.add(stringRequest); // Añade el request.
+                }
+            }
+        });
     }
 
     @Override
     public void onMapLongClick(LatLng latLng) {
-
-
         Utils.coordenadas.setOrigenLat(latLng.latitude);
         Utils.coordenadas.setDestinoLat(latLng.longitude);
         Toast.makeText(MainActivity.this,"Toque icono para que seleccione",Toast.LENGTH_SHORT).show();
@@ -219,7 +371,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
+    private void TrazarRuta(JSONObject jso) {
+        JSONArray jRoutes;
+        JSONArray jLegs;
+        JSONArray jSteps;
 
+        try {
+            jRoutes = jso.getJSONArray("routes");
+            for (int i = 0; i < jRoutes.length(); i++) {
+                jLegs = ((JSONObject) (jRoutes.get(i))).getJSONArray("legs");
+
+                for (int j = 0; j < jLegs.length(); j++) {
+                    jSteps = ((JSONObject) jLegs.get(j)).getJSONArray("steps");
+
+                    for (int k = 0; k < jSteps.length(); k++) {
+                        String polyline = "" + ((JSONObject) ((JSONObject) jSteps.get(k)).get("polyline")).get("points");
+                        Log.i("END", "" + polyline);
+                        List<LatLng> list = PolyUtil.decode(polyline);
+                        nMap.addPolyline(new PolylineOptions().addAll(list).color(Color.GREEN).width(5));
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 
@@ -334,4 +510,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(i);
     }
+
 }
